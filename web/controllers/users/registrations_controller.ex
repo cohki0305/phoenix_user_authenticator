06c961c6b7662dfeb9ff6298ba94ee6dfc115c7e
cohki0305 <cohki0305@gmail.com>
2017-09-30 14:25:55 +0900

    add create action in registrations controller

--- web/controllers/users/registrations_controller.ex
+++ web/controllers/users/registrations_controller.ex
@@ -6,4 +6,22 @@ defmodule UserAuthenticator.Users.RegistrationsController do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case User.create(changeset, Repo) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "ようこそ" <> changeset.params["email"])
+        |> redirect(to: redirect_path_after_registration(conn))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "アカウントを作成できませんでした")
+        |> redirect(to: registrations_path(conn, :new))
+    end
+  end
+
+  defp redirect_path_after_registration(conn) do
+    page_path(conn, :index)
+  end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,6 +4,7 @@ defmodule UserAuthenticator.User do
   schema "users" do
     field :email, :string
     field :crypted_password, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -13,7 +14,24 @@ defmodule UserAuthenticator.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :crypted_password])
-    |> validate_required([:email, :crypted_password])
+    |> cast(params, [:email, :password])
+    |> validate_required([:email, :password])
+    |> update_change(:email, &String.downcase/1)
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+  end
+
+  @doc """
+  Create user record after encrypting password
+  """
+
+  def create(changeset, repo) do
+    changeset
+    |> put_change(:crypted_password, hashed_password(changeset.params["password"]))
+    |> repo.insert()
+  end
+
+  defp hashed_password(password) do
+    Comeonin.Bcrypt.hashpwsalt(password)
   end
 end
