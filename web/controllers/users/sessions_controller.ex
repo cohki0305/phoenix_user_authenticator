d9301ad5b75cccfd5b5e06fae528fc05a45110a5
cohki0305 <cohki0305@gmail.com>
2017-10-01 10:42:38 +0900

    user can login

--- web/controllers/users/sessions_controller.ex
+++ web/controllers/users/sessions_controller.ex
@@ -1,7 +1,26 @@
 defmodule UserAuthenticator.Users.SessionsController do
   use UserAuthenticator.Web, :controller
+  alias UserAuthenticator.Session
 
   def new(conn, _params) do
     render conn, "new.html"
+  end
+
+  def create(conn, %{"session" => session_params}) do
+    case Session.create(session_params, Repo) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user, user.id)
+        |> put_flash(:info, "ログインしました")
+        |> redirect(to: redirect_path_after_login(conn))
+      :error ->
+        conn
+        |> put_flash(:error, "ログインに失敗しました")
+        |> render("new.html")
+    end
+  end
+
+  defp redirect_path_after_login(conn) do
+    page_path(conn, :index)
   end
 end
